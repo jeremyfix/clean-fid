8f7223dcc49fe81afd8ef2d7a476751a05835acb
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-09-10 18:24:14 +0200

    definition of a wrapper, compatible with the clean_fid resizers

--- cleanfid/utils.py
+++ cleanfid/utils.py
@@ -6,6 +6,58 @@ from cleanfid.resize import build_resizer
 import zipfile
 
 
+class ResizeWrapperDataset(torch.utils.data.Dataset):
+    """
+    A wrapper around a dataset
+    """
+
+    def __init__(self, dataset, mode, size=(299, 299)):
+        super().__init__()
+        self.base_dataset = dataset
+        self.transforms = torchvision.transforms.ToTensor()
+        self.fn_resize = build_resizer(mode)
+        self.custom_image_tranform = lambda x: x
+
+    def __len__(self):
+        return len(self.bae_dataset)
+
+    def __getitem__(self, i):
+        elem_i = self.base_dataset[i]
+
+        # elem_i is expected to be either a tuple of 1 or 2 elements
+        # dependening on whether or not the dataset is labeled
+        if isinstance(elem_i, tuple):
+            if len(elem_i) == 1:
+                input_i = elem_i[0]
+            elif len(elem_i) == 2:
+                input_i, _ = elem_i
+            else:
+                raise RuntimeError(
+                    "A mappable dataset returning more than one element is not handled"
+                )
+        else:
+            input_i = elem_i
+
+        # The dataset is expected to return a tensor (C, H, W)
+        # with values in [0, 1], e.g. MNIST dataset with transforms.ToTensor
+        if not isinstance(input_i, torch.Tensor):
+            raise RuntimeError(
+                f"Expected the dataset to return a torch.Tensor, got {type(input_i)} instead"
+            )
+        if len(input_i.shape) != 3:
+            raise RuntimeError(
+                f"Expected the input tensor to be 3-dimensional, got {len(input_i.shape)} dimensional tensor instead"
+            )
+
+        # Convert the pytorch tensor to a nd array in [0, 255]
+        # The resizer is expecting a nd array in this range
+        np_input_i = 255 * input_i.numpy()
+        img_resized = self.fn_resize(np_input_i)
+        img_t = self.transforms(img_resized)
+
+        return img_t
+
+
 class ResizeDataset(torch.utils.data.Dataset):
     """
     A placeholder Dataset that enables parallelizing the resize operation
